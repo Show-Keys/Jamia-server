3faef9041953b4eacb87c7fc3f1c1e12bb0255ef
Show-Key <127041780+Show-Keys@users.noreply.github.com>
2024-12-22 08:36:42 +0400

    Update wheelRoutes.js

--- routes/wheelRoutes.js
+++ routes/wheelRoutes.js
@@ -5,12 +5,12 @@ import { getWheelItems, addWheelItem, deleteWheelItem } from '../controllers/whe
 const router = express.Router();
 
 // GET all wheel items
-router.get('/', getWheelItems);
+router.get('/getWheelItems', getWheelItems);
 
 // POST a new wheel item
-router.post('/', addWheelItem);
+router.post('/addWheelItem', addWheelItem);
 
 // DELETE a wheel item by ID
-router.delete('/:id', deleteWheelItem);
+router.delete('/deleteWheelItem:id', deleteWheelItem);
 
 export default router;
